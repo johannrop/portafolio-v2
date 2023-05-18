3c4d48a0baec078295f199844d5726330dccb1f2
johann <johann.ortiz@unipamplona.edu.co>
2023-05-17 21:16:59 -0500

    mejorada UI en el header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  mostrarEnlaces : boolean = false;
+
+  largeWindon: boolean = false;
+
+
+
+  ngOnInit(): void {
+    if(!this.mostrarEnlaces) this.onWindowResize(null);
+    
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: any) {
+    this.mostrarEnlaces = window.innerWidth > 600;
+    console.log(this.mostrarEnlaces)
+ 
+    
+
+  }
+
+  cambiarMenu(){
+    this.mostrarEnlaces=true;
+    console.log(this.mostrarEnlaces)
+  }
+
+  containerInicio(){
+    this.mostrarEnlaces=false;
+    console.log(this.mostrarEnlaces)
+  }
+
+  containerConocimientos(){
+    this.mostrarEnlaces=false;
+  }
+
+  containerProyectos(){
+    this.mostrarEnlaces=false;
+  }
 
 }
