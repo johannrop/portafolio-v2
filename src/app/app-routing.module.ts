646f362bb0e87870bf511ca4c578cce834d7c932
johann <johann.ortiz@unipamplona.edu.co>
2023-05-19 23:05:32 -0500

    generado el componente footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Routes = [
   {path:'app-header', component:HeaderComponent},
+  {path:'app-footer', component:FooterComponent},
 ];
 
 @NgModule({
